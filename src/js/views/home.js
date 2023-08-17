d8f3ae5bde96db4e1ec8e92f5cbeee32ac5d9a4b
Appa1715 <128950756+Appa1715@users.noreply.github.com>
2023-08-17 01:22:35 +0000

    started

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,12 +4,28 @@ import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+		<div className="card" style={{width: 500,}}>
+			<img src="..." className="card-img-top" alt="..."></img>
+			<div className="card-body">
+				<h5 className="card-title">Card title</h5>
+				<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				<a href="#" className="btn btn-primary">Go somewhere</a>
+			</div>
+		</div>
+		<div className="card" style={{width: 500,}}>
+			hola
+		</div>
+		<div className="card" style={{width: 500,}}>
+			hola
+		</div>
+		<div className="card" style={{width: 500,}}>
+			hola
+		</div>
+		<div className="card" style={{width: 500,}}>
+			hola
+		</div>
+		<div className="card " style={{width: 500,}}>
+			hola
+		</div>
 	</div>
 );
